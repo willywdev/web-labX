95692f8cba9d13d3165b6cec3bcac4a2c2d972c3
Wilhelm Werwein <werwein.dev@gmail.com>
2023-07-12 18:18:05 +0200

    change color theme

--- js/localStorage.js
+++ js/localStorage.js
@@ -3,7 +3,7 @@ export const darkModeToggle = document.querySelector('[data-js="darkmode"]');
 
 const theme = localStorage.getItem("theme");
 if (theme === "dark") {
-  root.style.setProperty("--bg-color", "#000");
+  root.style.setProperty("--bg-color", "#252629");
   darkModeToggle.checked = true;
 }
 theme === "light" ? root.style.setProperty("--bg-color", "#fff") : "";
