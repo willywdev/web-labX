631e5f98ee865809fb9fa93e6dc3bfe0384beec9
Wilhelm Werwein <werwein.dev@gmail.com>
2023-07-12 21:23:07 +0200

    change theme

--- js/localStorage.js
+++ js/localStorage.js
@@ -3,7 +3,12 @@ export const darkModeToggle = document.querySelector('[data-js="darkmode"]');
 
 const theme = localStorage.getItem("theme");
 if (theme === "dark") {
-  root.style.setProperty("--bg-color", "#252629");
+  root.style.setProperty("--bg-color", "#0f0e17");
+  root.style.setProperty("--secondary-color", "#F5f5f4");
+  root.style.setProperty("--headline-color", "#0f0e17");
+  root.style.setProperty("--paragraph-color", "#0f0e17");
+  root.style.setProperty("--dark-bg-color", "#F5f5f4");
+
   darkModeToggle.checked = true;
 }
-theme === "light" ? root.style.setProperty("--bg-color", "#fff") : "";
+theme === "light" ? root.style.setProperty("--bg-color", "#F5f5f4") : "";
