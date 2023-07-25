64f597f00f026a45d8c533e9cfd69d507b1acc7c
Wilhelm Werwein <werwein.dev@gmail.com>
2023-07-25 10:28:02 +0200

    create functionality for answer button

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const bookmarkButton = document.querySelector('[data-js="bookmarkButton"]');
 const bookmarkImage = document.querySelector('[data-js="bookmarkImage"]');
 const answerButton = document.querySelector('[data-js="answerButton"]');
 const answerText = document.querySelector('[data-js="answerText"]');
+
 bookmarkButton.addEventListener("click", () => {
   if (bookmarkImage.src.includes("bookmark-fill.svg")) {
     bookmarkImage.src = "../assets/bookmark-line.svg";
@@ -9,7 +10,11 @@ bookmarkButton.addEventListener("click", () => {
     bookmarkImage.src = "../assets/bookmark-fill.svg";
   }
 });
-
 answerButton.addEventListener("click", () => {
-  console.log("Hello World!d");
+  if (answerButton.textContent.includes("Show")) {
+    answerButton.textContent = "Hide Answer";
+  } else if (answerButton.textContent.includes("Hide")) {
+    answerButton.textContent = "Show Answer";
+  }
+  answerText.classList.toggle("hidden");
 });
