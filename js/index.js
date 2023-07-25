6f0d225f2a429825ea9eb35abee04c06f32b7367
Wilhelm Werwein <werwein.dev@gmail.com>
2023-07-25 10:21:28 +0200

    html:create p for answer+add data-js, js:get dom elements+create event

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 const bookmarkButton = document.querySelector('[data-js="bookmarkButton"]');
 const bookmarkImage = document.querySelector('[data-js="bookmarkImage"]');
-
+const answerButton = document.querySelector('[data-js="answerButton"]');
+const answerText = document.querySelector('[data-js="answerText"]');
 bookmarkButton.addEventListener("click", () => {
   if (bookmarkImage.src.includes("bookmark-fill.svg")) {
     bookmarkImage.src = "../assets/bookmark-line.svg";
   } else {
     bookmarkImage.src = "../assets/bookmark-fill.svg";
   }
+});
+
+answerButton.addEventListener("click", () => {
+  console.log("Hello World!d");
 });
