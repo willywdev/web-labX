7adebf6e503d0db009416f04a1e0c0d1def3374e
Wilhelm Werwein <werwein.dev@gmail.com>
2023-07-25 10:14:07 +0200

    change source of bookmark image when clicked

--- js/index.js
+++ js/index.js
@@ -2,5 +2,9 @@ const bookmarkButton = document.querySelector('[data-js="bookmarkButton"]');
 const bookmarkImage = document.querySelector('[data-js="bookmarkImage"]');
 
 bookmarkButton.addEventListener("click", () => {
-  console.log(bookmarkButton);
+  if (bookmarkImage.src.includes("bookmark-fill.svg")) {
+    bookmarkImage.src = "../assets/bookmark-line.svg";
+  } else {
+    bookmarkImage.src = "../assets/bookmark-fill.svg";
+  }
 });
