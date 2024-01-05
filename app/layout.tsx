5e15ead6e2b8fe52a4b4e44c9c414e9391ca79eb
willywdev <werwein.dev@gmail.com>
2024-01-05 13:06:20 +0100

    Update layout and configuration files

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,12 @@ export const metadata: Metadata = {
   description: "Test your skills in web development",
 };
 
+const bodyStyle = `${GeistSans.className} w-full h-full bg-neutral-100 text-neutral-950 dark:bg-neutral-950 dark:text-neutral-100`;
+
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en' className='w-full h-full'>
-      <body className={GeistSans.className}>{children}</body>
+    <html lang='en' className='dark'>
+      <body className={bodyStyle}>{children}</body>
     </html>
   );
 }
