6b97a6977947a0d34c5708bc764f56f8cc57abae
willywdev <werwein.dev@gmail.com>
2024-01-06 14:12:43 +0100

    Refactor Sidebar component to include a fixed sidebar with a gray background

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,5 +1,11 @@
+import Island from "../Island";
+
 function Sidebar() {
-  return <p>Sidebar</p>;
+  return (
+    <aside className='fixed top-0 left-0 w-1/6 h-full bg-gray-500'>
+      Sidebar
+    </aside>
+  );
 }
 
 export default Sidebar;
